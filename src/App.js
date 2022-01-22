87f88ca290b85d26dda73f86f79ed18822aef297
Clucas0311 <clucas0311@gmail.com>
2022-01-22 18:53:48 -0500

    Header

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="app">
+      {/* Header */}
+      <Header />
+
+      {/* App body */}
+      {/* SideBar */}
+      {/* Feed */}
+      {/* Widgits */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/768px-LinkedIn_logo_initials.png"
+          alt=""
+        />
+        <div className="header__search">
+          <SearchIcon />
+          <input type="text" />
+        </div>
+      </div>
+      <div className="header__right"></div>
+    </div>
+  );
+};
+
+export default Header;
