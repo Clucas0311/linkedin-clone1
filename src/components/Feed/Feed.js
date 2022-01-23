adf97713675388cf29f4bc6f94c1d11e6e571854
Clucas0311 <clucas0311@gmail.com>
2022-01-23 18:57:58 -0500

    Removed Aligned-item center

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./Feed.css";
+import CreateIcon from "@mui/icons-material/Create";
 
 const Feed = () => {
-  return <div className="feed"></div>;
+  return (
+    <div className="feed">
+      <div className="feed__inputContainer">
+        <CreateIcon />
+        <form action="">
+          <input type="text" />
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Feed;
