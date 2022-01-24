2674f8a2efc56f201623d1966eaa8863aeae942f
Clucas0311 <clucas0311@gmail.com>
2022-01-23 19:24:56 -0500

    Added Image Icon

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./Feed.css";
+import InputOption from "./InputOption";
 import CreateIcon from "@mui/icons-material/Create";
 import ImageIcon from "@mui/icons-material/Image";
-import InputOption from "./InputOption";
-
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import EventNoteIcon from "@mui/icons-material/EventNote";
+import CalendarViewDayIcon from "@mui/icons-material/CalendarViewDay";
 const Feed = () => {
   return (
     <div className="feed">
@@ -17,6 +19,19 @@ const Feed = () => {
         </div>
         <div className="feed__inputOptions">
           <InputOption Icon={ImageIcon} title="Photo" color="#70BFf9" />
+        </div>
+        <div className="feed__inputOptions">
+          <InputOption Icon={SubscriptionsIcon} title="Video" color="#E7A33E" />
+        </div>
+        <div className="feed__inputOptions">
+          <InputOption Icon={EventNoteIcon} title="Event" color="#C0CBCD" />
+        </div>
+        <div className="feed__inputOptions">
+          <InputOption
+            Icon={CalendarViewDayIcon}
+            title="Write article"
+            color="#7FC15E"
+          />
         </div>
       </div>
     </div>
